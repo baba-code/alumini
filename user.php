7eea05178f94b73ab79c3fcf6732c504e306ee28
watchdogs54 <43605478+watchdogs54@users.noreply.github.com>
2018-09-26 17:00:27 +0530

    Add files via upload
    Database file

---
+++ user.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 0.2b
+ */
+
+//
+// Database `alumini`
+//
+
+// `alumini`.`user`
+$user = array(
+  array('name' => 'Dd','batch' => '1997-1999','email' => '','contact' => '0','add1' => '','add2' => '','city' => '','state' => '','pin' => '0','country' => '','inst_comp' => 'Institute','nature' => '','lcity' => ''),
+  array('name' => 'Dd','batch' => '1997-1999','email' => '','contact' => '0','add1' => '','add2' => '','city' => '','state' => '','pin' => '0','country' => '','inst_comp' => 'Institute','nature' => '','lcity' => ''),
+  array('name' => 'Dd','batch' => '1997-1999','email' => '','contact' => '0','add1' => '','add2' => '','city' => '','state' => '','pin' => '0','country' => '','inst_comp' => 'Institute','nature' => '','lcity' => ''),
+  array('name' => 'Yash','batch' => 'batch','email' => 'email','contact' => '101','add1' => '1234','add2' => 'Hh','city' => 'delhi','state' => 'Uttar Pradesh','pin' => '110046','country' => 'India','inst_comp' => 'Institute','nature' => 'IIC','lcity' => 'delhi'),
+  array('name' => 'Deepak','batch' => 'batch','email' => 'email','contact' => '100','add1' => '11','add2' => ' Andher ','city' => 'Delhi','state' => 'Delhi','pin' => '110046','country' => 'India','inst_comp' => 'Institute','nature' => 'IIC','lcity' => 'Hawala'),
+  array('name' => 'Pathak','batch' => 'batch','email' => 'email','contact' => '100','add1' => '1234','add2' => '','city' => 'delhi','state' => 'Uttar Pradesh','pin' => '110046','country' => 'India','inst_comp' => 'Company','nature' => 'IIC','lcity' => 'Delhi'),
+  array('name' => 'Sanjeev chauhan','batch' => 'batch','email' => 'email','contact' => '100','add1' => 'B22/227 A','add2' => '','city' => 'Varanasi','state' => 'Uttar Pradesh','pin' => '221010','country' => 'India','inst_comp' => 'Institute','nature' => 'IIC','lcity' => 'Varanasi')
+);
